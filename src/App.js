e8a3877822c3359f58bf5b2a6e1a59d67cc4c3e3
matheus <matheusnun.ns@gmail.com>
2025-01-30 20:02:38 -0300

    chore: create react project

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h2>Caderno de exercícios</h2>
+      </header>
+      <div>
+        <h3>Exercício 1</h3>
+        <p>Escreva um programa que imprima “Hello, World!” na tela.</p>
+        <code>
+          <pre>
+            {`console.log("Hello, World!");`}
+          </pre>
+        </code>
+      </div>
+    </div>
+  );
+}
+
+export default App;
